aba1ffb951b05b57a4cafb7bac568db2af5464ae
CleWri00 <clewri163@gmail.com>
2026-09-06 16:37:07 -0400

    Notes on downloading requirements.txt

--- scripts/setup.ps1
+++ scripts/setup.ps1
@@ -11,6 +11,12 @@ py -3.11 -m venv .venv
 python -m pip install --upgrade pip
 python -m pip install -r requirements.txt
 
+# You may need to run the following as the pnpl github ReadeMe has a unicode character that cannot be read 
+# Error msg: UnicodeDecodeError: 'charmap' codec can't decode byte 0x8d
+
+# $env:PYTHONUTF8="1"
+# python -m pip install -r requirements.txt
+
 Write-Host ""
 Write-Host "Setup complete."
 Write-Host "Activate with: .venv\Scripts\Activate.ps1"
